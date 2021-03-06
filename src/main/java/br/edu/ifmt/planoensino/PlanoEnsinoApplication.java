0cb51c6dd2a3e8f7102ef2135fb1cd48aaffdf74
Willian Maltus <willianmaltus@bol.com.br>
2021-03-06 14:38:41 -0400

    commit inicial com o projeto base do spring criado.

---
+++ src/main/java/br/edu/ifmt/planoensino/PlanoEnsinoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifmt.planoensino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanoEnsinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanoEnsinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifmt/planoensino/PlanoEnsinoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifmt.planoensino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanoEnsinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
